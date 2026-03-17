bd6001f40e22a63199199db043ef9cdbc1e0823b
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2026-03-17 17:29:51 +0700

    refactor: update Header component layout and remove unused ChevronLeft icon

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { ChevronLeft } from "lucide-react";
 
 export default function Header() {
   return (
@@ -15,15 +14,13 @@ export default function Header() {
       <div className="absolute inset-0 bg-linear-to-b from-black/60 via-black/10 to-transparent z-10"></div>
 
       <div className="relative z-20 pt-10 px-6 h-full flex flex-col justify-between pb-24">
-        <div className="flex items-center justify-between text-white drop-shadow-md">
-          <ChevronLeft size={24} className="cursor-pointer" />
+        <div className="flex items-center justify-center text-white drop-shadow-md">
           <div className="text-center">
             <h1 className="text-xl font-bold">Halo, Admin</h1>
             <p className="text-xs opacity-90 font-medium">
               Sistem Manajemen Peternakan
             </p>
           </div>
-          <div className="w-6"></div>
         </div>
       </div>
     </div>
