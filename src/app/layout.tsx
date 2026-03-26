743554b7737b2d3f5540b6456a263bf61807b5e0
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2026-03-26 16:07:50 +0700

    feat: implement react hot toast for notification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import ConditionalBottomNav from "@/shared/components/ConditionalBottomNav";
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Toaster position="top-center" reverseOrder={false} />
         <div className="bg-gray-200 min-h-screen flex justify-center font-sans">
           <div className="w-full max-w-md bg-slate-50 min-h-screen relative shadow-2xl flex flex-col overflow-hidden">
             {children}
