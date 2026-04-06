968d63283518947bb3c7a2d4f3232a11fd694491
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2026-04-06 11:34:23 +0700

    feat: update BottomNavigation to conditionally display Stock link based on user authentication and role, enhancing navigation options for users

--- src/shared/components/BottomNavigation.tsx
+++ src/shared/components/BottomNavigation.tsx
@@ -25,10 +25,12 @@ export default function BottomNavigation() {
   };
 
   const roleName = user?.roles?.[0]?.name?.toLowerCase() || "";
+
   const showActivity = ["admin", "manager", "karyawan"].includes(roleName);
+  const showStock = ["admin", "manager", "karyawan"].includes(roleName);
 
   return (
-    <div className="fixed bottom-0 max-w-md z-9999 w-full bg-white flex justify-between items-end px-6 py-4 rounded-t-[30px] shadow-[0_-10px_40px_rgb(0,0,0,0.08)]">
+    <div className="fixed bottom-0 max-w-md z-9999 w-full bg-white flex justify-evenly items-end py-4 rounded-t-[30px] shadow-[0_-10px_40px_rgb(0,0,0,0.08)]">
       <Link
         href="/"
         className="flex flex-col items-center gap-1 cursor-pointer group">
@@ -57,18 +59,20 @@ export default function BottomNavigation() {
         </Link>
       )}
 
-      <Link
-        href="/stock-susu"
-        className="flex flex-col items-center gap-1 cursor-pointer group">
-        <FileText
-          size={24}
-          className={`transition-colors duration-300 ${checkActive("/stock-susu") ? "text-emerald-600" : "text-gray-400 group-hover:text-emerald-500"}`}
-        />
-        <span
-          className={`text-[10px] font-medium transition-colors duration-300 ${checkActive("/stock-susu") ? "text-emerald-600" : "text-gray-400 group-hover:text-emerald-500"}`}>
-          Stock
-        </span>
-      </Link>
+      {isClient && isAuthenticated && showStock && (
+        <Link
+          href="/stock-susu"
+          className="flex flex-col items-center gap-1 cursor-pointer group animate-in fade-in zoom-in duration-300">
+          <FileText
+            size={24}
+            className={`transition-colors duration-300 ${checkActive("/stock-susu") ? "text-emerald-600" : "text-gray-400 group-hover:text-emerald-500"}`}
+          />
+          <span
+            className={`text-[10px] font-medium transition-colors duration-300 ${checkActive("/stock-susu") ? "text-emerald-600" : "text-gray-400 group-hover:text-emerald-500"}`}>
+            Stock
+          </span>
+        </Link>
+      )}
 
       <Link
         href="/profile"
