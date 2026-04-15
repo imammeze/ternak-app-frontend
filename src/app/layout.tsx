4b1080a423c269f1c1c358eded6341121a387753
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2026-04-15 16:39:45 +0700

    fix: update Toaster component in RootLayout to include zIndex for improved visibility and positioning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,13 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Toaster position="top-center" reverseOrder={false} />
+        <Toaster
+          position="top-center"
+          containerStyle={{
+            zIndex: 9999999,
+          }}
+          reverseOrder={false}
+        />
         <div className="bg-gray-200 min-h-screen flex justify-center font-sans">
           <div className="w-full max-w-md bg-slate-50 min-h-screen relative shadow-2xl flex flex-col overflow-hidden">
             {children}
